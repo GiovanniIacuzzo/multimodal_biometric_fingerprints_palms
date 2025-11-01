51c355dbad39d43bfbdfb9a9925c6e40d605fda4
GiovanniIacuzzo <iacuzzogiovanni02@gmail.com>
2025-11-01 16:02:34 +0100

    inserimento del .env

--- src/config.py
+++ src/config.py
@@ -1,17 +1,30 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Carica variabili da .env
+load_dotenv(dotenv_path=Path(__file__).parent / "config.env")
 
 # -------------------------------
 # CONFIGURAZIONE
 # -------------------------------
-DATASET_DIR = Path("../dataset/DBII")
-OUTPUT_CSV = Path("labels.csv")
+DATASET_DIR = Path(os.getenv("DATASET_DIR", "../dataset/DBII"))
+OUTPUT_CSV = Path(os.getenv("OUTPUT_CSV", "labels.csv"))
 
-IMG_SCALES = [(128,128),(64,64),(32,32)]
-BLOCK_SIZE = 32
-GAUSSIAN_BLUR = 5
-USE_HOG = True
-USE_GABOR = True
-USE_LBP = True
-MAX_K_CLUSTERS = 5
-CONFIDENCE_THRESHOLD = 5
-MAX_SAMPLES_PER_CLASS = 200
+# Lettura scale multiple
+IMG_SCALES = [
+    (int(os.getenv("IMG_SCALE_1", 128)), int(os.getenv("IMG_SCALE_1", 128))),
+    (int(os.getenv("IMG_SCALE_2", 64)), int(os.getenv("IMG_SCALE_2", 64))),
+    (int(os.getenv("IMG_SCALE_3", 32)), int(os.getenv("IMG_SCALE_3", 32)))
+]
+
+BLOCK_SIZE = int(os.getenv("BLOCK_SIZE", 32))
+GAUSSIAN_BLUR = int(os.getenv("GAUSSIAN_BLUR", 5))
+
+USE_HOG = os.getenv("USE_HOG", "True") == "True"
+USE_GABOR = os.getenv("USE_GABOR", "True") == "True"
+USE_LBP = os.getenv("USE_LBP", "True") == "True"
+
+MAX_K_CLUSTERS = int(os.getenv("MAX_K_CLUSTERS", 5))
+CONFIDENCE_THRESHOLD = float(os.getenv("CONFIDENCE_THRESHOLD", 5))
+MAX_SAMPLES_PER_CLASS = int(os.getenv("MAX_SAMPLES_PER_CLASS", 200))
